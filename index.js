4dc30c8ae180fe2a7997746fb761d8db0bdea36d
Nasser691 <138252255+Nasser691@users.noreply.github.com>
2025-04-17 23:22:32 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 require('dotenv').config(); // لتحميل المتغيرات من ملف .env
 const { Client } = require('discord.js-selfbot-v13');
 const { joinVoiceChannel } = require('@discordjs/voice');
+const express = require('express'); // <-- إضافة Express
+const app = express();
+const port = process.env.PORT || 3000;
+
+// سيرفر وهمي لـ Render
+app.get('/', (req, res) => {
+  res.send('Bot is running!');
+});
+app.listen(port, () => {
+  console.log(`Express server running on port ${port}`);
+});
 
 const client = new Client();
 
 client.on('ready', async () => {
   console.log(`${client.user.username} is ready!`);
-  
+
   const channelId = process.env.CHANNEL_ID;
   const guildId = process.env.GUILD_ID;
 
